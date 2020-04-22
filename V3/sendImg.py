fea0dfc0855100ea4ca5fa915f81fbad3baaea36
Joash <32333304+Joash-JW@users.noreply.github.com>
2020-04-22 17:08:59 +0800

    finalise sendImg.py

--- V3/sendImg.py
+++ V3/sendImg.py
@@ -11,58 +11,23 @@ class sendImg:
     self.host = host
     self.port = port
     self.count = 0
-#    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    self.serversocket.bind((self.host, self.port))
-#    self.serversocket.listen(1) #only connect up to 1 request
 
     # camera initialisation
     self.camera = PiCamera()
     self.camera.resolution = (640, 480)
     self.output = PiRGBArray(self.camera)
 
-    # accept connection and extract IP address
-#    (self.clientsocket, self.address) = self.serversocket.accept()
-#    print("Connected to Image Reg PC")
-#    self.cleanUp()
-
     # start camera preview to let camera warm up
     self.camera.start_preview()
     threading.Thread(target=time.sleep, args=(2,)).start()
 
-  def connect(self):
-    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.serversocket.bind((self.host, self.port))
-    self.serversocket.listen(1) #only connect up to 1 request
-
-    (self.clientsocket, self.address) = self.serversocket.accept()
-    print("Connected to Image Reg PC")
-    self.cleanUp()
-
-  def cleanUp(self):
-    # free resources
-    self.clientsocket.close()
-    self.serversocket.close()
-
-  def run(self):
-    while True:
-      self.camera.capture(self.output, 'bgr')
-      img_arr = self.output.array
-      data = pickle.dumps(img_arr)
-      # send to PC/Laptop via HTTP POST
-      r = requests.post("http://"+str(self.address[0])+":8123", data=data)
-      print('Image', self.count , 'sent')
-      self.output.truncate(0)
-      self.count += 1
-      time.sleep(0.5)
-
-  # this functions take 2 pictures every time the robot moves
+  # this function takes a picture when commanded
   def takePic(self):
     self.camera.capture(self.output, 'bgr')
     frame = self.output.array
     data = pickle.dumps(frame)
     # send to Laptop via HTTP POST
-    #r = requests.post("http://"+str(self.address[0])+":8123", data=data)
-    r = requests.post("http://192.168.16.133:8123", data=data) #static IP ver
+    r = requests.post("http://192.168.16.133:8123", data=data) #static IP
     print("Image", self.count, "sent")
     self.output.truncate(0)
     self.count+=1
